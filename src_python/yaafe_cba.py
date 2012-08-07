165de187866f8266c0c8089281f603a3f2c487ab
mcrg-fhstp <ewald.wieser@fhstp.ac.at>
2012-08-07 11:40:25 +0300

    correcting header

--- src_python/yaafe_cba.py
+++ src_python/yaafe_cba.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: ISO-8859-1 -*-
 #
-# Yaafe
-# 
-# Copyright (c) 2009-2010 Institut Télécom - Télécom Paristech
-# Télécom ParisTech / dept. TSI
-# 
-# Author : Benoit Mathieu
-# 
-# This file is part of Yaafe.
-# 
+# Yaafe extension for the Cultural Broadcasting Archive.
+#
+# Copyright (c) 2012 University of Applied Sciences â€“ Institute for Creative Media Technologies
+#
+# Author : Ewald Wieser B.Sc. (fhstp.ac.at)
+#
+# Co-Authors :
+#      Dipl.-Ing. (FH) Matthias Husinsky (fhstp.ac.at)
+#      FH-Prof. Dipl.-Ing. Markus Seidl (fhstp.ac.at)
+#
+# Contributes to : 
+#      Dr. Klaus Seyerlehner (jku.at)
+#
+# This file is an extension for Standard-Yaafe, developed for Music
+# detection with the Continuous Frequency Activation Feature (CFA) for
+# the Cultural Broadcasting Archive (CBA).
+#
 # Yaafe is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Yaafe is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
 
 import os
